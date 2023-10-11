9190f26d110022c1f13faec2ea15a810f2e79597
Raymundo Ycaza <raymundo.ycaza.m@gmail.com>
2023-10-11 12:03:10 -0500

    Replace remote pictures with local pictures

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://raymundoycaza.com.com",
+  site: "https://raymundoycaza.com",
   integrations: [
     tailwind(),
     image({
